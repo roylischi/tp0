d774f3b0b40e540420977f0d13b44e11b4cb4d7b
roylischi <rlischinsky@frba.utn.edu.ar>
2024-04-13 20:39:43 +0000

    primer commit tp0

--- server/src/server.c
+++ server/src/server.c
@@ -3,7 +3,7 @@
 int main(void) {
 	logger = log_create("log.log", "Servidor", 1, LOG_LEVEL_DEBUG);
 
-	int server_fd = iniciar_servidor();
+	int server_fd = iniciar_servidor(); //fd = file descriptor, es un int, permite distinguir una conexion de otra 
 	log_info(logger, "Servidor listo para recibir al cliente");
 	int cliente_fd = esperar_cliente(server_fd);
 
--- server/src/utils.c
+++ server/src/utils.c
@@ -5,26 +5,28 @@ t_log* logger;
 int iniciar_servidor(void)
 {
 	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
+	// assert(!"no implementado!");
 
 	int socket_servidor;
 
-	struct addrinfo hints, *servinfo, *p;
+	struct addrinfo hints, *servinfo;
 
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
-	getaddrinfo(NULL, PUERTO, &hints, &servinfo);
+	getaddrinfo(NULL, PUERTO, &hints, &serverinfo);
 
 	// Creamos el socket de escucha del servidor
-
+	int fd_conexion = socket(serverinfo->ai_family,
+                         serverinfo->ai_socktype,
+                         servinfo->ai_protocol);
 	// Asociamos el socket a un puerto
-
+	bind(fd_escucha, serverinfo->ai_addr, servinfo->ai_addrlen);
 	// Escuchamos las conexiones entrantes
-
-	freeaddrinfo(servinfo);
+	listen(socket_servidor, SOMAXCONN);
+	freeaddrinfo(serverinfo);
 	log_trace(logger, "Listo para escuchar a mi cliente");
 
 	return socket_servidor;
@@ -36,7 +38,7 @@ int esperar_cliente(int socket_servidor)
 	assert(!"no implementado!");
 
 	// Aceptamos un nuevo cliente
-	int socket_cliente;
+	int socket_cliente = accept(socket_servidor, NULL, NULL);;
 	log_info(logger, "Se conecto un cliente!");
 
 	return socket_cliente;
