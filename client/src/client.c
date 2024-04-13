3db77725322a92015c744b35264e2a80186effaf
roylischi <rlischinsky@frba.utn.edu.ar>
2024-04-13 18:43:02 +0000

    finalizada leer_consola()

--- client/src/client.c
+++ client/src/client.c
@@ -15,9 +15,11 @@ int main(void)
 	/* ---------------- LOGGING ---------------- */
 
 	logger = iniciar_logger();
+	log_info(logger, "Hola! SOy un log.");
 
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
+
 
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
@@ -26,9 +28,14 @@ int main(void)
 
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
+	valor = config_get_string_value(config, "CLAVE"); 
 
 	// Loggeamos el valor de config
-
+	log_info(logger, "VALOR leido de la config: %s", valor);
+	//log_info(logger, "VALOR leido de la config: %s", puerto);
+	//log_info(logger, "VALOR leido de la config: %s", ip);
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
@@ -50,19 +57,27 @@ int main(void)
 
 	/*---------------------------------------------------PARTE 5-------------------------------------------------------------*/
 	// Proximamente
+	printf("\n¡¡ Cliente cerrado !!");
 }
 
 t_log* iniciar_logger(void)
 {
-	t_log* nuevo_logger;
+	t_log* nuevo_logger = log_create("cliente.log", "CL_LOG", 1, LOG_LEVEL_INFO);
+	if(nuevo_logger == NULL){
+		perror("ALgo raro paso con el log. NO se pudo crear o encontrar el archivo.");
+		exit(EXIT_FAILURE);
+	}
 
 	return nuevo_logger;
 }
 
 t_config* iniciar_config(void)
 {
-	t_config* nuevo_config;
-
+	t_config* nuevo_config = config_create("cliente.config");
+	if(nuevo_config == NULL){
+		perror("Error al intentar cargar el config");
+		exit(EXIT_FAILURE);
+	}
 	return nuevo_config;
 }
 
@@ -72,11 +87,17 @@ void leer_consola(t_log* logger)
 
 	// La primera te la dejo de yapa
 	leido = readline("> ");
+	log_info(logger, ">> %s", leido);
 
 	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
-
+	while(strcmp(leido, "") != 0){ //1=true, 0=false
+		free(leido);
+		leido = readline("> ");
+		log_info(logger, ">> %s", leido);
+	} 
 
 	// ¡No te olvides de liberar las lineas antes de regresar!
+	free(leido);
 
 }
 
@@ -97,4 +118,6 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	log_destroy(logger);
+	config_destroy(config);
 }
